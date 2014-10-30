d7fd4110effe8d1da6a663cc73b3096936eadae7
Robert Boll <robertcboll@gmail.com>
2014-10-30 17:19:00 -0400

    tagged version

--- src/main/g8/project/project/Build.scala
+++ src/main/g8/project/project/Build.scala
@@ -6,6 +6,6 @@ object PluginDef extends Build {
   override def projects = Seq(root)
 
   lazy val root = Project("plugins", file(".")) dependsOn 
-    RootProject(uri("ssh://stash.ondeck.local:7999/sch/sbt-base.git"))
+    RootProject(uri("ssh://stash.ondeck.local:7999/sch/sbt-base.git#v0.1"))
 }
 
