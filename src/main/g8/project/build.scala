06d85514c25de52beb1cc0663e13508c9f6ff040
Robert Boll <robertcboll@gmail.com>
2014-12-12 20:57:25 -0500

    niceties

--- src/main/g8/project/build.scala
+++ src/main/g8/project/build.scala
@@ -6,19 +6,18 @@ object Build extends sbt.Build {
 
   import sbtbase._
   import Templates._
-  import ExtraSettings._
-  import plugin.Migrations._
 
   import com.typesafe.sbt.SbtGit._
   import GitKeys._
 
-
+  /*                   */
+  /* common settings   */
+  /*                   */
   val repoBase = "$repo_base$"
   val resolverBase = "$resolver_base$"
   val gitverBase = "$gitver_base$"
   val releaseBase = "$release_base$"
   val mavenRelease = $maven_release$
-
 
   override lazy val settings = super.settings ++
     Seq(
@@ -26,6 +25,7 @@ object Build extends sbt.Build {
       name := "$name$",
       scalaVersion := "$version_scala$",
       resolvers += "$resolver_name$" at s"\$repoBase\$resolverBase",
+      publishMavenStyle := mavenRelease,
       credentials += Credentials(Path.userHome / ".sbt" / ".credentials"),
       publishTo <<= (version, gitHeadCommit) { (version, gitHeadCommit) =>
         val resolverType = if (mavenRelease) Resolver.mavenStylePatterns else Resolver.ivyStylePatterns
@@ -35,20 +35,42 @@ object Build extends sbt.Build {
         val publishRepoBase = if (gitver) gitverBase else releaseBase
 
         Some(Resolver.url("publish", url(s"\$repoBase\$publishRepoBase"))(resolverType))
-      },
-      publishMavenStyle := mavenRelease
+      }
     )
 
+  /*                      */
+  /* packaging settings   */
+  /*                      */
+  import com.typesafe.sbt.SbtNativePackager._
+  import NativePackagerKeys._
+
+  import com.typesafe.sbt.packager._
+  import archetypes.ServerLoader.SystemV
+
+  val packaging = packageArchetype.java_server ++ Seq(
+    maintainer in Linux := "$maintainer$",
+    packageSummary in Linux := "$package_summary$",
+    packageDescription := "$package_description$",
+    
+    serverLoading in Debian := SystemV,
+    packageBin in Debian <<= debianJDebPackaging in Debian,
+    mappings in Universal <++= sourceDirectory map { src => Seq(
+      src / "main" / "resources" / "reference.conf" -> "conf/application.conf",
+      src / "main" / "resources" / "logback.xml" -> "conf/logback.xml"
+      )
+    }
+  )
+
+  /*                      */
+  /* project definitions  */
+  /*                      */
   import Dependencies.Projects
 
   lazy val root = RootProject("$name$")
     .aggregate(/* add new modules here */)
-    .settings(migrations: _*)
+    .settings(plugin.Migrations.migrations: _*)
     .settings(giter8.ScaffoldPlugin.scaffoldSettings: _*)
     .settings(libraryDependencies ++= Dependencies.migrations)
-
-  import com.typesafe.sbt.SbtNativePackager._
-  import NativePackagerKeys._
 
   /*
   // documentation project
@@ -58,75 +80,28 @@ object Build extends sbt.Build {
   /*
   // java project
   lazy val java = JavaProject("$name$-java", deps = Seq(scala))
-    .settings(libraryDependencies ++= Dependencies.javaDeps)
+    .settings(libraryDependencies ++= .Projects.javaDeps)
   */
 
   /*
   // scala project
   lazy val scala = ScalaProject("$name$-scala", deps = Seq(java))
-    .settings(libraryDependencies ++= Dependencies.scalaDeps)
+    .settings(libraryDependencies ++= Projects.scalaDeps)
   */
 
   /*
   // app project
   lazy val runnable = JavaProject("$name$-app", deps = Seq(java, scala))
-    .settings(libraryDependencies ++= Dependencies.appDeps)
-    .settings(runnable: _*)
-    .settings(targz: _*)
+    .settings(libraryDependencies ++= Projects.appDeps)
+    .settings(ExtraSettings.runnable: _*)
+    .settings(packaging: _*)
     .settings(mainClass := Some("mainClass"))
   */
 
- /*
-  // artifactory 
-  import com.typesafe.sbt.SbtGit._
-  import GitKeys._
-  
-  val repo = ""
-  val repoName = ""
-  val creds = Credentials(Path.userHome / ".sbt" / ".credentials")
-
-  lazy val artifactory = Seq(
-    resolvers += "internal" at repo + "repo"
-    )
-
-  lazy val mavenPublish = Seq(
-    publishTo <<= (version, gitHeadCommit) { (version, gitHeadCommit) =>
-      val snapshotName = s"\$repoName-gitver"
-      val releaseName = s"\$repoName-releases"
-
-      val snapshots = Some(Resolver.url(snapshotName, url(repo + snapshotName))(Resolver.mavenStylePatterns))
-      val releases = Some(Resolver.url(releaseName, url(repo + releaseName))(Resolver.mavenStylePatterns))
-
-      gitHeadCommit match {
-        case Some(commit) => 
-          if (version.endsWith(commit)) snapshots
-          else releases
-        case None => releases
-      }
-    },
-    publishMavenStyle := true,
-    credentials += creds
-    )
-
-  lazy val ivyPublish = Seq(
-    publishTo <<= (version, gitHeadCommit) { (version, gitHeadCommit) =>
-      val snapshotName = s"\$repoName-ivy-gitver"
-      val releaseName = s"\$repoName-ivy-release"
-
-      val snapshots = Some(Resolver.url(snapshotName, url(artifactory + snapshotName))(Resolver.ivyStylePatterns))
-      val releases = Some(Resolver.url(releaseName, url(artifactory + releaseName))(Resolver.ivyStylePatterns))
-
-      gitHeadCommit match {
-        case Some(commit) => 
-          if (version.endsWith(commit)) snapshots
-          else releases
-        case None => releases
-      }
-    },
-    publishMavenStyle := false,
-    credentials += creds
-    )
-
- */
+  /* 
+    // for sequential (integration) tests
+    .settings(parallelExecution in IntegrationTest := false)
+    .settings(parallelExecution in itJacoco.Config := false)
+   */
 }
 
